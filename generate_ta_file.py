4108f80950880c888e5d00ff07c67d61b151be24
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-02-04 18:35:05 -0500

    writing common part

--- generate_ta_file.py
+++ generate_ta_file.py
@@ -1,6 +1,18 @@
 import understand
 
+def open_file_for_write():
+    ta_file = open("nginx_architecture.ta", "w")
+    return ta_file
+
+def write_common_part():
+    ta_file = open_file_for_write()
+    common_part = open("common_part.txt")
+    for line in common_part.readlines():
+        ta_file.write(line)
+    ta_file.write('\n')
+
 db = understand.open("nginx.udb")
-for file in db.ents("file"):
+# for file in db.ents("file"):
     # if source in file.longname():
-    print(file.longname())
+    # print(file.longname())
+write_common_part()

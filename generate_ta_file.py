f3714a7b498a494d54961371348bbe2926edd515
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-02-04 19:49:43 -0500

    write contain

--- generate_ta_file.py
+++ generate_ta_file.py
@@ -1,5 +1,6 @@
 import understand
 import pathlib
+import os
 
 def open_file_for_write():
     ta_file = open("nginx_architecture.ta", "w")
@@ -19,10 +20,36 @@ def write_instance(ta_file, db):
     current_path = pathlib.Path(__file__).parent.absolute()
 
     for file in db.ents("file"):
-        relative_path = file.longname()[len(str(current_path)):]
-        relative_path = relative_path.replace("\\","/")
-        ta_file.write("$INSTANCE " + relative_path + " cFile")
-        ta_file.write('\n')
+        if(str(current_path) in file.longname()):
+            relative_path = file.longname()[len(str(current_path)):]
+            relative_path = relative_path.replace("\\","/")
+            ta_file.write("$INSTANCE " + relative_path + " cFile")
+            ta_file.write('\n')
+    
+    folders = [x[0] for x in os.walk(os.path.join(current_path, "sources", "nginx"))]
+    for folder in folders:
+        if(str(current_path) in folder):
+            folder_path = folder[len(str(current_path)):]
+            folder_path = folder_path.replace("\\","/")
+            ta_file.write("$INSTANCE " + folder_path + " cSubSystem")
+            ta_file.write('\n')
+
+def cleanup_path(path, root_to_remove):
+    clean_path = path[len(str(root_to_remove)):]
+    return clean_path.replace("\\","/")
+
+
+def write_contain(ta_file, db):
+    current_path = pathlib.Path(__file__).parent.absolute()
+    folders = [x[0] for x in os.walk(os.path.join(current_path, "sources", "nginx"))]
+    for folder in folders:
+        for root,dirs,files in os.walk(folder):
+            for sub_dir in dirs:
+                ta_file.write("contain " + cleanup_path(root, str(current_path)) + " " + cleanup_path(root, str(current_path)) + sub_dir)
+                ta_file.write('\n')
+            for file_path in files:
+                ta_file.write("contain " + cleanup_path(root, str(current_path)) + " " + cleanup_path(root, str(current_path)) + file_path)
+                ta_file.write('\n')
 
 db = understand.open("nginx.udb")
 # for file in db.ents("file"):
@@ -31,3 +58,4 @@ db = understand.open("nginx.udb")
 ta_file = open_file_for_write()
 write_common_part(ta_file)
 write_instance(ta_file, db)
+write_contain(ta_file, db)

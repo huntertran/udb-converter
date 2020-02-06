31bbc153a95c9090ae93113d75b1cb4701f9528e
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2020-02-06 14:59:39 -0500

    move to class

--- generate_ta_file.py
+++ generate_ta_file.py
@@ -2,69 +2,83 @@ import understand
 import pathlib
 import os
 
-def open_file_for_write():
-    ta_file = open("nginx_architecture.ta", "w")
-    return ta_file
 
-def clear_content(ta_file):
-    ta_file.truncate(0)
+class understand_to_lsedit_converter(object):
 
-def write_common_part(ta_file):
-    common_part = open("common_part.txt")
-    for line in common_part.readlines():
-        ta_file.write(line)
-    ta_file.write('\n')
+    db = ""
+    ta_file = ""
 
-def write_instance(ta_file, db):
+    def open_file_for_write(self):
+        ta_file = open("nginx_architecture.ta", "w")
+        return ta_file
 
-    current_path = pathlib.Path(__file__).parent.absolute()
+    def clear_content(self):
+        self.ta_file.truncate(0)
 
-    for file in db.ents("file"):
-        # if(str(current_path) in file.longname()):
-        # relative_path = file.longname()[len(str(current_path)):]
-        relative_path = file.relname().replace("\\","/")
-        ta_file.write("$INSTANCE " + relative_path + " cFile")
-        ta_file.write('\n')
-    
-    folders = [x[0] for x in os.walk(os.path.join(current_path, "sources", "nginx"))]
-    for folder in folders:
-        if(str(current_path) in folder):
-            folder_path = folder[len(str(current_path)):]
-            folder_path = folder_path.replace("\\","/")
-            ta_file.write("$INSTANCE " + folder_path + " cSubSystem")
-            ta_file.write('\n')
+    def write_common_part(self):
+        common_part = open("common_part.txt")
+        for line in common_part.readlines():
+            self.ta_file.write(line)
+            self.ta_file.write('\n')
 
-def cleanup_path(path, root_to_remove):
-    clean_path = path[len(str(root_to_remove)):]
-    return clean_path.replace("\\","/")
+    def write_instance(self):
+        current_path = pathlib.Path(__file__).parent.absolute()
+
+        for file in self.db.ents("file"):
+            # if(str(current_path) in file.longname()):
+            # relative_path = file.longname()[len(str(current_path)):]
+            relative_path = file.relname().replace("\\", "/")
+            self.ta_file.write("$INSTANCE " + relative_path + " cFile")
+            self.ta_file.write('\n')
+
+        folders = [x[0]
+                   for x in os.walk(os.path.join(current_path, "sources", "nginx"))]
+        for folder in folders:
+            if(str(current_path) in folder):
+                folder_path = folder[len(str(current_path)):]
+                folder_path = folder_path.replace("\\", "/")
+                self.ta_file.write("$INSTANCE " + folder_path + " cSubSystem")
+                self.ta_file.write('\n')
+
+    def cleanup_path(self, path, root_to_remove):
+        clean_path = path[len(str(root_to_remove)):]
+        return clean_path.replace("\\", "/")
+
+    def write_contain(self):
+        current_path = pathlib.Path(__file__).parent.absolute()
+        folders = [x[0]
+                   for x in os.walk(os.path.join(current_path, "sources", "nginx"))]
+        for folder in folders:
+            for root, dirs, files in os.walk(folder):
+                for sub_dir in dirs:
+                    self.ta_file.write("contain " + self.cleanup_path(root, str(current_path)) +
+                                  " " + self.cleanup_path(root, str(current_path)) + sub_dir)
+                    self.ta_file.write('\n')
+                for file_path in files:
+                    self.ta_file.write("contain " + self.cleanup_path(root, str(current_path)) +
+                                  " " + self.cleanup_path(root, str(current_path)) + file_path)
+                    self.ta_file.write('\n')
+
+    def write_clinks(self):
+        current_path_str = str(pathlib.Path(__file__).parent.absolute())
+        for file in self.db.ents("file"):
+            if(current_path_str.lower() in file.longname().lower()):
+                file_path = self.cleanup_path(file.longname(), current_path_str)
+                related_paths = file.depends()
+                for related_path in related_paths:
+                    self.ta_file.write("cLinks " + file.relname() +
+                                  " " + related_path.relname())
+                    self.ta_file.write('\n')
+
+    def convert(self, udb_file_path):
+        self.db = understand.open(udb_file_path)
+        self.ta_file = self.open_file_for_write()
+        self.write_common_part()
+        self.write_instance()
+        self.write_contain()
+        self.write_clinks()
+        self.ta_file.close()
 
 
-def write_contain(ta_file, db):
-    current_path = pathlib.Path(__file__).parent.absolute()
-    folders = [x[0] for x in os.walk(os.path.join(current_path, "sources", "nginx"))]
-    for folder in folders:
-        for root,dirs,files in os.walk(folder):
-            for sub_dir in dirs:
-                ta_file.write("contain " + cleanup_path(root, str(current_path)) + " " + cleanup_path(root, str(current_path)) + sub_dir)
-                ta_file.write('\n')
-            for file_path in files:
-                ta_file.write("contain " + cleanup_path(root, str(current_path)) + " " + cleanup_path(root, str(current_path)) + file_path)
-                ta_file.write('\n')
-
-def write_clinks(ta_file, db):
-    current_path_str = str(pathlib.Path(__file__).parent.absolute())
-    for file in db.ents("file"):
-        if(current_path_str.lower() in file.longname().lower()):
-            file_path = cleanup_path(file.longname(), current_path_str)
-            related_paths = file.depends()
-            for related_path in related_paths:
-                ta_file.write("cLinks " + file.relname() + " " + related_path.relname())
-                ta_file.write('\n')
-
-db = understand.open("nginx.udb")
-ta_file = open_file_for_write()
-write_common_part(ta_file)
-write_instance(ta_file, db)
-write_contain(ta_file, db)
-write_clinks(ta_file,db)
-ta_file.close()
+converter = understand_to_lsedit_converter()
+converter.convert("nginx.udb")
